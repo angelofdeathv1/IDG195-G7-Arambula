12538dcaa58e4ea53205242747e3c86bf70167fa
Angel Arambula <angel.arambula@thermofisher.com>
2018-08-09 19:28:52 -0700

    Added list prime number identification

--- 08-09-Actividad/PrimeNumber.java
+++ 08-09-Actividad/PrimeNumber.java
@@ -2,6 +2,14 @@ public class PrimeNumber {
 
     public static void main(String[] args) {
         System.out.println(isPrime(6));
+
+        int[] myArray = {4,5,11,2,6,7,3,8};
+
+        for (int i=0;i<myArray.length; i++){
+            if (isPrime(myArray[i])){
+                System.out.println(i+" is a Prime Number!");
+            }
+        }
     }
 
     public static boolean isPrime(int number) {
