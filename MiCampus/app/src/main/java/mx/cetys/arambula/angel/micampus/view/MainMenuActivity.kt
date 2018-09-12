8a1e4ab09910fb521dd88fe8c1196953fc7ceba9
Angel Arambula <angel.arambula@thermofisher.com>
2018-09-11 19:35:49 -0700

    added navigation

--- MiCampus/app/src/main/java/mx/cetys/arambula/angel/micampus/view/MainMenuActivity.kt
+++ MiCampus/app/src/main/java/mx/cetys/arambula/angel/micampus/view/MainMenuActivity.kt
@@ -2,6 +2,8 @@ package mx.cetys.arambula.angel.micampus.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.NavUtils
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main_menu.*
 import mx.cetys.arambula.angel.micampus.R.layout.activity_main_menu
 import mx.cetys.arambula.angel.micampus.model.Perfil
@@ -11,7 +13,19 @@ class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_main_menu)
+
+        actionBar.setDisplayHomeAsUpEnabled(true)
         val perfil = intent.getParcelableExtra<Perfil>("perfil")
         txt_logged_user.text = perfil.carrera + " " + perfil.apellido + " " + perfil.nombre
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                NavUtils.navigateUpFromSameTask(this)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
